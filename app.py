b7c5a9b33e36e205e9fce7cde4198187332e9d04
QtyPython2020 <htm131016@gmail.com>
2024-01-18 15:19:32 +0100

    Layout test

--- app.py
+++ app.py
@@ -6,10 +6,13 @@
 # Standard library
 
 # Third party
+import plotly.graph_objects as go
 import streamlit as st
 # Local imports
 
 TITLE = "Activity Mapper"
+
+temporary_figure = go.Figure()
 
 def main():
     with st.spinner("Making visualizations..."):
@@ -23,17 +26,17 @@ def main():
         # top row
         left, right = st.columns(spec=[6,6],
                                  gap="small")
-        left.plotly_chart(figure_or_data=None,
+        left.plotly_chart(figure_or_data=go.Figure(),
                           use_container_width=True)
-        right.plotly_chart(figure_or_data=None,
+        right.plotly_chart(figure_or_data=temporary_figure,
                            use_container_width=True)
         # middel row
         cols = left.columns([2,1,1])
-        cols[0].plotly_chart(figure_or_data=None,
+        cols[0].plotly_chart(figure_or_data=temporary_figure,
                              use_container_width=True)
-        cols[1].plotly_chart(figure_or_data=None,
+        cols[1].plotly_chart(figure_or_data=temporary_figure,
                              use_container_width=True)
-        cols[2].plotly_chart(figure_or_data=None,
+        cols[2].plotly_chart(figure_or_data=temporary_figure,
                              use_container_width=True)
     with st.container():
         st.write("")
