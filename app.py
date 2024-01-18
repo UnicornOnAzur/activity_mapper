2ce78276199f99afa27149a3b4830df561767939
QtyPython2020 <htm131016@gmail.com>
2024-01-18 14:24:36 +0100

    Retry of streamlit app

---
+++ app.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@author: QtyPython2020
+
+"""
+# Standard library
+
+# Third party
+import streamlit as st
+# Local imports
+
+TITLE = "Activity Mapper"
+
+def main():
+    with st.spinner("Making visualizations..."):
+        # sidebar
+        with st.sidebar:
+            st.header("Menu")
+
+    # MAIN PAGE
+    with st.container():
+        st.header(TITLE)
+
+
+if __name__ == "__main__":
+    # set the initial state of the sidebar
+    if "sidebar_state" not in st.session_state:
+        st.session_state["sidebar_state"] = "expanded"
+    st.set_page_config(page_title=TITLE,
+                       page_icon=":world_map:",
+                       layout="wide",
+                       initial_sidebar_state=st.session_state.get("sidebar_state")
+                       )
+    main()
