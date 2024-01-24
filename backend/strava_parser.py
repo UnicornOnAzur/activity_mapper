bc99ba9f8d0244318addc3d16d5b62f8c7ce962d
QtyPython2020 <htm131016@gmail.com>
2024-01-24 21:49:21 +0100

    Add changes to parser

--- backend/strava_parser.py
+++ backend/strava_parser.py
@@ -5,11 +5,58 @@
 """
 
 # Standard library
+import requests
 import typing
 # Third party
 import pandas as pd
 import polyline
 # Local imports
+
+
+def request_data_from_api(access_token: str) -> list[dict]:
+    """
+    Send get requests in a loop to retreive all the activities. Loop will stop if
+    the result is empty implying that all activities have been retrieved.
+
+    Parameters
+    ----------
+    access_token : str
+        DESCRIPTION.
+
+    Returns
+    -------
+    list[dict]
+        DESCRIPTION.
+
+    """
+    activities_url = "https://www.strava.com/api/v3/athlete/activities"
+    header = {"Authorization": f"Bearer {access_token}"}
+    request_page_num = 1
+    all_activities = []
+
+    while True:
+        param = {"per_page": 200,
+                 "page": request_page_num}
+        response = requests.get(activities_url,
+                                headers=header,
+                                params=param)
+        data_set = response.json()
+        print(request_page_num, response.status_code, type(data_set))
+        if not response.ok:
+            all_activities.append(data_set)
+            return all_activities
+        # break out of the loop if the response is empty
+        if len(data_set) == 0:
+            break
+        # add onto the list
+        if all_activities:
+            all_activities.extend(data_set)
+        # populate the list if it is empty
+        else:
+            all_activities = data_set
+        # increment to get the next page
+        request_page_num += 1
+    return all_activities
 
 
 def get_lat_long(value: list[float]) -> list[typing.Union[None,float]]:
