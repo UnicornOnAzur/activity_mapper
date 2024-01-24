c39dc2b422bd53ddecb10f1bacb2a33cfb093369
QtyPython2020 <htm131016@gmail.com>
2024-01-24 16:24:52 +0100

    Removed additional API calls

--- app.py
+++ app.py
@@ -37,86 +37,11 @@ def get_access_token(authorization_code):
     st.session_state["refresh_token"] = res.get("refresh_token")
     st.session_state["access_token"] = res.get("access_token")
 
-def request_access_token(client_id: str,
-                         client_secret: str,
-                         refresh_token: str) -> str:
-    """
-    Send a post request to the Strava API to get an access token. In case the response
-    is not ok an empty string is return.
-
-    Parameters
-    ----------
-    client_id : str
-        The Strava Client ID.
-    client_secret : str
-        The Strava Secret code.
-    refresh_token : str
-        The Strava Refresh token.
-
-    Returns
-    -------
-    token: str
-        The token from the response or an empty string.
-
-    """
-    token : str = ""
-
-    auth_url : str = "https://www.strava.com/oauth/token"
-    payload : dict = {"client_id": client_id,
-                      "client_secret": client_secret,
-                      "refresh_token": refresh_token,
-                      "grant_type": "refresh_token",
-                      "f": "json"}
-
-    response = requests.post(auth_url,
-                             data=payload,
-                             verify=False)
-    if response.ok:
-        token = response.json()['access_token']
-        return token
-    return token
-
-def retrieve_activities():
-    activities_url = "https://www.strava.com/api/v3/athlete/activities"
-    header = {"Authorization": f"Bearer {st.session_state['access_token']}"}
-    request_page_num = 1
-    all_activities = []
-
-    while True:
-        st.write(f"{request_page_num=}")
-        param = {"per_page": 200,
-                 "page": request_page_num}
-        response = requests.get(activities_url,
-                                headers=header,
-                                params=param)
-        if response.status_code == 401:
-            st.session_state["access_token"] = request_access_token(STRAVA_CLIENT_ID,
-                                                                    STRAVA_CLIENT_SECRET,
-                                                                    st.session_state["refresh_token"])
-            continue
-        data_set = response.json()
-        st.write(request_page_num, response.status_code, type(data_set))
-        if not response.ok:
-            all_activities.append(data_set)
-            return all_activities
-        # break out of the loop if the response is empty
-        if len(data_set) == 0:
-            break
-        # add onto the list
-        if all_activities:
-            all_activities.extend(data_set)
-        # populate the list if it is empty
-        else:
-            all_activities = data_set
-        # increment to get the next page
-        request_page_num += 1
-    return all_activities
-
 def connect(code):
     st.write("getting access code")
     get_access_token(code)
     st.write("getting activities")
-    st.write(retrieve_activities())
+    # st.write(retrieve_activities())
     st.write("done")
 
 
