06a38f2dfaee2cb2e60c3b39b478d4ddbd76650e
QtyPython2020 <htm131016@gmail.com>
2024-01-25 21:29:10 +0100

    Changed preprocessing of mapbox

--- backend/plotly_charts.py
+++ backend/plotly_charts.py
@@ -227,7 +227,7 @@ def worldmap_figure(data: pd.DataFrame,
                     **kwargs):
     lats = kwargs.get("lat", [])
     lons = kwargs.get("lon", [])
-    # colors = kwargs.get("color", [])
+    colors = kwargs.get("color", [])
     names = kwargs.get("name", [])
     dates = kwargs.get("date", [])
     times = kwargs.get("time", [])
@@ -235,7 +235,7 @@ def worldmap_figure(data: pd.DataFrame,
     figure = px.line_mapbox(data_frame=data,
                             lat=lats,
                             lon=lons,
-                            color=["Strava"]*len(lats),
+                            color=colors,
                             hover_name=names,
                             custom_data=[names, dates, times],
                             color_discrete_map=COLOR_MAP,
@@ -427,7 +427,7 @@ def locations(dataframe: pd.DataFrame,
 
         lats.append(None)
         lons.append(None)
-        colors.append(None)
+        colors.append(row["app"])
         names.append(None)
         dates.append(None)
         times.append(None)
