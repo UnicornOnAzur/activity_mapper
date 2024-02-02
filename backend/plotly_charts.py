e7493e37efcdbca069d2d098faef7d9f65d333eb
QtyPython2020 <htm131016@gmail.com>
2024-02-02 21:36:16 +0100

    Change x-axis label on timeline back to previous version

--- backend/plotly_charts.py
+++ backend/plotly_charts.py
@@ -161,6 +161,7 @@ def timeline_figure(aggregated_data: pd.DataFrame,
                                  yaxis: False
                                  },
                      custom_data=["app"],
+                     labels={xaxis: "Year"},
                      color_discrete_map=COLOR_MAP,
                      orientation="v", # orient the chart
                      line_shape="spline", # smoothes out the line
@@ -440,7 +441,7 @@ def timeline(original: pd.DataFrame,
                             plot_height)
     # prepare data
     summarize_name = "Times per week"
-    name = "Year"
+    name = "calender-week"
     dataframe = original.copy()
     dataframe["pos"] = 0
     last = None
