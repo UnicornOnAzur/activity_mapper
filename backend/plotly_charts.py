eef8eace81c1aa6799b3b1596fe97318aee0dbef
QtyPython2020 <htm131016@gmail.com>
2024-02-03 22:52:00 +0100

    Refactoring

--- backend/plotly_charts.py
+++ backend/plotly_charts.py
@@ -234,8 +234,7 @@ def weekdays_figure(aggregated_data: pd.DataFrame,
                     height=height,
                     **kwargs
                     )
-    figure.update_traces(hovertemplate="<b>%{customdata[0]}</b><br>%{y}"
-                         )
+    figure.update_traces(hovertemplate="<b>%{customdata[0]}</b><br>%{y}")
     figure.update_layout(xaxis={"tickmode": "array",
                                 "tickvals": list(range(7)),
                                 # relabel the xaxis ticks
@@ -337,11 +336,7 @@ def sunburst_figure(aggregated_data: pd.DataFrame,
                          template=TEMPLATE,
                          height=height,
                          **kwargs)
-    figure.update_traces(#textposition="inside", # set the percentage inside the chart
-    #                      # show the category and the amount in the chart
-    #                      textinfo="label+percent",
-                          hovertemplate="<b>%{label}</b><br>%{value}"
-                          )
+    figure.update_traces(hovertemplate="<b>%{label}</b><br>%{value}")
     figure = _update_layout(figure)
     return figure
 
