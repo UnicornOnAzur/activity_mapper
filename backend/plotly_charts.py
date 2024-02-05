dbd3ac28d37457ef2397409c5abe2e9e4a857ade
QtyPython2020 <htm131016@gmail.com>
2024-02-05 16:47:02 +0100

    Debugging _update_layout even more

--- backend/plotly_charts.py
+++ backend/plotly_charts.py
@@ -85,7 +85,7 @@ def _update_layout(fig: go.Figure,
     xaxis = {"fixedrange": True}
     if "xaxis" in kwargs:
         xaxis.update(kwargs.get("xaxis"))
-    print(xaxis)
+    print(kwargs)
     fig.update_layout(showlegend=False,
                       # TODO: add useful comment
                       margin={"l": LEFT_RIGHT_MARGIN,
