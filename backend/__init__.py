93c6c59e245fc7c2e86ae232d61d6ae1bdbdcf0b
QtyPython2020 <htm131016@gmail.com>
2024-02-25 21:44:29 +0100

    Reordered imports for backend

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,11 @@
-from backend.plotly_charts import (
-    days,
-    hours,
-    locations,
-    timeline,
-    types
+from backend.utils import (
+    get_request,
+    hr2ang,
+    load_category_mapper,
+    load_country_code_mapper,
+    load_image,
+    min2ang,
+    post_request,
     )
 
 from backend.resources import (
@@ -33,14 +35,17 @@ from backend.resources import (
 from backend.strava_parser import (
     get_access_token,
     get_country,
+    parse
     )
 
-from backend.utils import (
-    get_request,
-    hr2ang,
-    load_category_mapper,
-    load_country_code_mapper,
-    load_image,
-    min2ang,
-    post_request,
+from backend.plotly_charts import (
+    days,
+    hours,
+    locations,
+    timeline,
+    types
+    )
+
+from backend.test import (
+    load_test_data
     )
