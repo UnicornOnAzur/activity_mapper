e340f12cc3ddf6b4964ce671b1add668589c39a2
QtyPython2020 <htm131016@gmail.com>
2024-02-27 21:58:46 +0100

    Added type hinting and more readable layout

--- backend/resources.py
+++ backend/resources.py
@@ -8,72 +8,78 @@ import plotly.express as px
 # Local imports
 import backend
 
-# text
-CAPTION = \
+# TEXT
+CAPTION: str = \
     """
 This page was created by QtyPython2020 and the code can be found on
 https://github.com/QtyPython2020/activity_mapper .
 """
-EXPLANATION =\
+EXPLANATION: str =\
     """
 To use this dashboard click on "Connect with Strava". This will redirect you to
 the Strava page. Then select to view public and/or private activities, and
 click "Authorize". This provides the dashboard with your data for the duration
 of your use.
 """
-ERROR_MESSAGE =\
+ERROR_MESSAGE: str =\
     """
 An error occurred while retrieving the data. Please try to authorize again.
 """
-TITLE = "Activity Mapper"
-# column lists
-DISPLAY_COLS = ["name",
-                "view on Strava",
-                "date",
-                "type",
-                "sport_type",
-                "country"]
-STRAVA_COLS = ["app",
-               "weekday",
-               "time",
-               "hour",
-               "minutes",
-               "name",
-               "date",
-               "country",
-               "lat",
-               "lon"]
-# config dicts
-CONFIG = {"displaylogo": False,
-          "displayModeBar": False}
-CONFIG2 = {"displaylogo": False,
-           "modeBarButtonsToRemove": ["pan2d",
-                                      "lasso2d",
-                                      "select2d",
-                                      "toImage",
-                                      "zoom2d",
-                                      "autoscale"]}
+TITLE: str = "Activity Mapper"
 
-# paths
-PATH_CODES = "country_codes.txt"
-PATH_MAPPER = "strava_categories.txt"
-# colors and themes
-COLOR_MAP = {"Strava": "#FC4C02"}  # the color of the Strava app
-DISCRETE_COLOR = px.colors.sequential.Oranges_r
-TEMPLATE = "plotly_dark"
-# sizes
-LEFT_RIGHT_MARGIN = 20
-TOP_BOTTOM_MARGIN = 25
-TOP_ROW_HEIGHT = 200
-BOTTOM_ROW_HEIGHT = 600
-# urls
-APP_URL = "https://strava-activity-mapper.streamlit.app/"
-AUTH_LINK = "https://www.strava.com/oauth/token"
-NOMINATIM_LINK = "https://nominatim.openstreetmap.org/reverse"
-# external data
-GEOJSON = backend.get_request(
+# COLUMNS FOR DATAFRAMA
+DISPLAY_COLS: list[str] = ["name",
+                           "view on Strava",
+                           "date",
+                           "type",
+                           "sport_type",
+                           "country"]
+STRAVA_COLS: list[str] = ["app",
+                          "weekday",
+                          "time",
+                          "hour",
+                          "minutes",
+                          "name",
+                          "date",
+                          "country",
+                          "lat",
+                          "lon"]
+
+# DICT WITH CONFIGURATION FOR PLOTLY CHARTS
+CONFIG: dict = {"displaylogo": False,  # remove the plotly logo
+                "displayModeBar": False  # modebar never visible
+                }
+CONFIG2: dict = {"displaylogo": False,  # remove the plotly logo
+                 "modeBarButtonsToRemove":  # remove buttons from modebar
+                 ["pan2d",  # pan button
+                  "toImage",  # download button
+                  ]
+                 }
+
+# FILE PATHS
+PATH_CODES: str = "country_codes.txt"
+PATH_MAPPER: str = "strava_categories.txt"
+
+# COLORS AND THEMES
+COLOR_MAP: dict = {"Strava": "#FC4C02"}  # the color of the Strava app
+DISCRETE_COLOR: list[str] = px.colors.sequential.Oranges_r
+TEMPLATE: str = "plotly_dark"
+
+# SIZES FOR PLOTS
+LEFT_RIGHT_MARGIN: int = 20
+TOP_BOTTOM_MARGIN: int = 25
+TOP_ROW_HEIGHT: int = 200
+BOTTOM_ROW_HEIGHT: int = 600
+
+# URLS
+APP_URL: str = "https://strava-activity-mapper.streamlit.app/"
+AUTH_LINK: str = "https://www.strava.com/oauth/token"
+NOMINATIM_LINK: str = "https://nominatim.openstreetmap.org/reverse"
+
+# JSON
+GEOJSON: dict = backend.get_request(
     "https://datahub.io/core/geo-countries/r/0.geojson"
-                              )
+                                    )
 
 
 if __name__ == "__main__":
