7fb04ec25e65ac7b2c8ea5f3e7a91aa8a5de1ac1
QtyPython2020 <htm131016@gmail.com>
2024-03-01 21:36:55 +0100

    Revert

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ def connect_strava(code: str):
     # RETREIVING THE DATA
     progress_bar.progress(33, "Retreiving data...")
     data = backend.thread_get_and_parse(st.session_state["access_token"])
-    # if an error occur stop the function
-    if data == []: #[0] == {'401': 'Unauthorized'}:
-        error_message = st.error(backend.ERROR_MESSAGE)
-        return
+    # # if an error occur stop the function
+    # if data == []: #[0] == {'401': 'Unauthorized'}:
+    #     error_message = st.error(backend.ERROR_MESSAGE)
+    #     return
     # PARSING THE DATA
     progress_bar.progress(67, "Parsing data...")
     # dataframe = backend.parse(data)
