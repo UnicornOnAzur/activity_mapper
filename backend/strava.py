39ec93fbda174e95dd3cc0e671df9b83f8b7ffbe
QtyPython2020 <htm131016@gmail.com>
2024-03-24 21:46:11 +0100

    bedig

--- backend/strava.py
+++ backend/strava.py
@@ -126,7 +126,8 @@ def nomatim_lookup(lat: str, lon: str) -> dict:
                                          params={"lat": lat,
                                                  "lon": lon,
                                                  "zoom": 3,  # country level
-                                                 "format": "json"}
+                                                 "format": "json"},
+                                         headers={} # TODO: ...
                                          )
     print(lat, lon, response)
     return response
@@ -229,24 +230,25 @@ def parse(activities: list[dict]) -> pd.DataFrame:
                              # activity as an unpacked tuple of the coordinates
                              # after mapping a rounding to 1 decimal and
                              # filling the strings to the length
-                             locate_country(*tuple(map(lambda x:
-                                                       # round the string to 1
-                                                       # decimal and store it
-                                                       (s := str(round(x, 1))
-                                                        ).ljust(
-                                                        # fill out the string
-                                                        # to the length of the
-                                                        # rounded string plus 2
-                                                        # characters
-                                                        len(s.split(".")[0])+2,
-                                                        # fill character
-                                                        "0"
-                                                                 ),
-                                                        [elements.get("lat"),
-                                                         elements.get("lon")]
-                                                        )
-                                                )
-                                             )
+                             # locate_country(*tuple(map(lambda x:
+                             #                           # round the string to 1
+                             #                           # decimal and store it
+                             #                           (s := str(round(x, 1))
+                             #                            ).ljust(
+                             #                            # fill out the string
+                             #                            # to the length of the
+                             #                            # rounded string plus 2
+                             #                            # characters
+                             #                            len(s.split(".")[0])+2,
+                             #                            # fill character
+                             #                            "0"
+                             #                                     ),
+                             #                            [elements.get("lat"),
+                             #                             elements.get("lon")]
+                             #                            )
+                             #                    )
+                             #                 )
+                             "Poland"
                              }
                             )
         parsed_activities.append(elements)
